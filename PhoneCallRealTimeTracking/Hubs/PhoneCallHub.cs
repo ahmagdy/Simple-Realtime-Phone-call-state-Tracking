d7d551f18e50a447f7f8c5037c14ba080ac67e50
Ahmad Magdy <ahmedmagdi@outlook.com>
2018-06-23 21:45:19 +0200

    use CORS and install signalR client package

---
+++ PhoneCallRealTimeTracking/Hubs/PhoneCallHub.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneCallRealTimeTracking.Hubs
+{
+    public class PhoneCallHub : Hub
+    {
+
+        public async Task StartCall(string connectionId, string something)
+        {
+            // Do Something
+            await Clients.Client(connectionId).SendAsync("ChangeCallState","Start");
+        }
+
+        public async Task ChangeCallState( string text)
+        {
+            var x = Context.ConnectionId;
+            var y = Context.UserIdentifier;
+
+            await Clients.Client(x).SendAsync(text);
+        }
+
+        public string GetConnectionId()
+        {
+            return Context.ConnectionId;
+        }
+
+
+    }
+}
--- PhoneCallRealTimeTracking/Startup.cs
+++ PhoneCallRealTimeTracking/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using PhoneCallRealTimeTracking.Hubs;
 
 namespace PhoneCallRealTimeTracking
 {
@@ -26,6 +27,14 @@ namespace PhoneCallRealTimeTracking
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors(options => options.AddPolicy("CorsPolicy",
+            builder =>
+            {
+                builder.AllowAnyMethod().AllowAnyHeader()
+                       .WithOrigins("http://localhost:4200")
+                       .AllowCredentials();
+            }));
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +49,12 @@ namespace PhoneCallRealTimeTracking
                 app.UseHsts();
             }
 
+            app.UseCors("CorsPolicy");
             app.UseHttpsRedirection();
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<PhoneCallHub>("/mapssss");
+            });
             app.UseMvc();
         }
     }
